12595eae99cff14e101b2cae0e491b5d02792b92
franklin-zheng <zheng.franklin@gmail.com>
2019-05-12 20:34:06 -0400

    Add StreamVideo
    I forgot to commit this file

---
+++ Assets/Personalitest/StreamVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+public class StreamVideo : MonoBehaviour
+{
+    public RawImage raw;
+    public VideoPlayer vp;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        vp.Prepare();
+        WaitForSeconds wait = new WaitForSeconds(1);
+        while (!vp.isPrepared)
+        {
+            yield return wait;
+            break;
+        }
+        raw.texture = vp.texture;
+        vp.Play();
+    }
+}
