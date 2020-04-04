74f54eeffd17549a4e5992c490c07e7ec6f05e83
franklin-zheng <zheng.franklin@gmail.com>
2020-04-04 15:21:27 -0400

    Update Questions, Plug in temp animations

---
+++ Assets/setSprites.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class setSprites : MonoBehaviour
+{
+    public Sprite myIdleSprite;
+    public Image myImage;
+
+    void LateUpdate()
+    {
+        myImage.overrideSprite = myIdleSprite;
+        myImage.sprite = myIdleSprite;
+    }
+}
