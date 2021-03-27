0de5a32f413553e2b82baa32fd50d48cefbcd273
franklin-zheng <zheng.franklin@gmail.com>
2021-03-27 13:19:16 -0400

    Initial server code. Connect on startup

--- Assets/AirConsole/scripts/editor/Extentions.cs
+++ Assets/AirConsole/scripts/editor/Extentions.cs
@@ -133,7 +133,7 @@ namespace NDream.AirConsole.Editor {
 		public static string GetLocalAddress () {
 			string localIP = "";
 
-			using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) {
+			using (System.Net.Sockets.Socket socket = new System.Net.Sockets.Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0)) {
 				socket.Connect("8.8.8.8", 65530);
 				IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
 				localIP = endPoint.Address.ToString();
---
+++ Assets/Personalitest/SocketClientFranklin.cs
@@ -0,0 +1,30 @@
+﻿using Socket.Quobject.SocketIoClientDotNet.Client;
+using UnityEngine;
+
+public class SocketClientFranklin : MonoBehaviour
+{
+    private QSocket socket;
+
+    // Start is called before the first frame update
+    public void Connect()
+    {
+        socket = IO.Socket("http://localhost:3000");
+        socket.On(QSocket.EVENT_CONNECT, () => {
+            Debug.Log("Connected");
+            socket.Emit("chat", "test");
+            socket.Emit("fas", "afsdfsa");
+            socket.Emit("afsddfsa", "asfdfdsafdas");
+            socket.Emit("afdsfadsadfsadfs", "dfasfadsfadsfd");
+            socket.Disconnect();
+        });
+
+        socket.On("chat", data => {
+            Debug.Log("data : " + data);
+        });
+        
+    }
+    private void OnDestroy()
+    {
+        socket.Disconnect();
+    }
+}
